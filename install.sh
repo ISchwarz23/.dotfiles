da0a6283d3ca500e0881f1d29549e0aaa5d5b10d
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-10-14 20:35:59 +0200

    make install script less verbose

--- install.sh
+++ install.sh
@@ -6,26 +6,21 @@ PACKAGES="$@"
 echo "[INFO] Going to install $PACKAGES..."
 
 if command -v pkg > /dev/null 2>&1; then
-    echo "[DEBUG] Using pkg..."
     apt-get update -y > /dev/null
     apt-get install -y $PACKAGES > /dev/null
 elif command -v apt > /dev/null 2>&1; then
-    echo "[DEBUG] Using apt..."
     sudo apt-get update -y > /dev/null
     sudo apt-get install -y $PACKAGES > /dev/null
     sudo apt-get autoremove -y > /dev/null
 elif command -v yum > /dev/null 2>&1; then
-    echo "[DEBUG] Using yum..."
     sudo yum makecache -y > /dev/null
     sudo yum install -y $PACKAGES > /dev/null
     sudo yum autoremove -y > /dev/null || true  # some yum versions lack autoremove
 elif command -v dnf > /dev/null 2>&1; then
-    echo "[DEBUG] Using dnf..."
     sudo dnf makecache -y > /dev/null
     sudo dnf install -y $PACKAGES > /dev/null
     sudo dnf autoremove -y > /dev/null
 elif command -v pacman > /dev/null 2>&1; then
-    echo "[DEBUG] Using pacman..."
     sudo pacman -Syu --noconfirm > /dev/null 2>&1 || true
     sudo pacman -S --noconfirm --needed $PACKAGES > /dev/null
 else
