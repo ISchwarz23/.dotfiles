ca2929fd40372d813d281568e3938b2e0d6745bd
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-11-28 23:11:48 +0100

    make nvim bg transparent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,3 +10,7 @@ vim.cmd[[syntax on]]
 
 -- enavle lazy vim
 require("config.lazy")
+
+-- make background transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
