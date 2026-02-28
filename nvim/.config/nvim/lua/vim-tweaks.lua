3fc3531aed9ff3314d55fb73fcbdde6946f74305
Ingo Schwarz <ingo.schwarz23@gmail.com>
2026-02-28 23:38:55 +0100

    add terminal keymaps to nvim

--- nvim/.config/nvim/lua/vim-tweaks.lua
+++ nvim/.config/nvim/lua/vim-tweaks.lua
@@ -17,6 +17,28 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- open terminal in a bottom split
+vim.keymap.set('n', '<leader>t', function()
+  vim.cmd("botright split")
+  vim.cmd("term")
+  vim.cmd("resize 10")
+end, { desc = "Open bottom terminal" })
+
+-- exit terminal mode 
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+
+-- automatically start insert mode when opening a terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.cmd("startinsert")
+    -- disable line numbers in terminal for a cleaner look
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+  end,
+})
+
 -- make background transparent
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
