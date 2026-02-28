35bbe88a5a2e8389709a6e044be5a52c9c4338ff
Ingo Schwarz <ingo.schwarz23@gmail.com>
2026-02-28 22:20:46 +0100

    add treesitter to nvim

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  branch = "master",
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = { "lua", "bash" },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true }
+    })
+  end
+}
