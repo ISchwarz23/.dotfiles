0632914a40c9a6b2f202e5fc654157db0f8a0b82
Ingo Schwarz <ingo.schwarz23@gmail.com>
2026-02-28 22:54:08 +0100

    add more tweaks to vim config

--- nvim/.config/nvim/lua/vim-tweaks.lua
+++ nvim/.config/nvim/lua/vim-tweaks.lua
@@ -1,9 +1,21 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
 -- enable relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- custom insert mode exit
 vim.keymap.set('i', 'jk', '<esc>', { desc = "Exit insert mode" })
+
+-- navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 -- make background transparent
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
