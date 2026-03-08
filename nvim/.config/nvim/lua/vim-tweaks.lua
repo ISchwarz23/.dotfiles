0548cdfc360a170e77e17beb0c7636f54f24249c
Ingo Schwarz <ingo.schwarz23@gmail.com>
2026-03-08 07:11:45 +0100

    remove redundant window transparency

--- nvim/.config/nvim/lua/vim-tweaks.lua
+++ nvim/.config/nvim/lua/vim-tweaks.lua
@@ -39,6 +39,3 @@ vim.api.nvim_create_autocmd("TermOpen", {
   end,
 })
 
--- make background transparent
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
